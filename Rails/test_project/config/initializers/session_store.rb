c7f9beee79200c1d52ef213328becfcc764e0dae
saada <mahmoudsaada@gmail.com>
2012-06-09 13:04:43 -0700

    fixed some bugs and edited Gemfile

--- Rails/test_project/config/initializers/session_store.rb
+++ Rails/test_project/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TestProject::Application.config.session_store :cookie_store, key: '_test_project_session'
+TestProject::Application.config.session_store :cookie_store, :key => '_test_project_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
