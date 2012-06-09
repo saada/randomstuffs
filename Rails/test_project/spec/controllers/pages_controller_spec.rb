c3a16fdac0301f2e4806f19b4c32c5f40a5487a7
saada <mahmoudsaada@gmail.com>
2012-06-09 14:41:24 -0700

    Added a site layout

--- Rails/test_project/spec/controllers/pages_controller_spec.rb
+++ Rails/test_project/spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "3AGALA.com | Home")
+    end
+    it "should have a non-blank body" do
+      get 'home'
+      #refer to Rublar.com for regexp help
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -14,12 +25,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "3AGALA.com | Contact")
+    end
   end
   
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "3AGALA.com | About")
     end
   end
 end
