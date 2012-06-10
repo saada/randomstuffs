f0b461bc190161109045d67153936f36339ef72d
saada <mahmoudsaada@gmail.com>
2012-06-10 16:06:02 -0700

    Added signup form

--- Rails/test_project/app/controllers/users_controller.rb
+++ Rails/test_project/app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   	@title = "Sign up"
   end
 
   def show
-  	@user = User.find(params[:id])
+  	@user = User.find(par ams[:id])
   	@title = @user.name
   end
 end
