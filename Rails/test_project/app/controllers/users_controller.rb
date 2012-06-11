3ad0ab4b20428d9e06230683d7fd2793ee2fc7bc
saada <mahmoudsaada@gmail.com>
2012-06-10 17:55:55 -0700

    Finished sign up

--- Rails/test_project/app/controllers/users_controller.rb
+++ Rails/test_project/app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 
 class UsersController < ApplicationController
+
   def new
   	@user = User.new
   	@title = "Sign up"
   end
 
   def show
-  	@user = User.find(par ams[:id])
+  	@user = User.find(params[:id])
   	@title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to 3AGALA!"
+      redirect_to @user
+    else
+	@title = "Sign up"
+	render 'new'
+    end
   end
 end
