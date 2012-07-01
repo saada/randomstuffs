97f15fe563b30f51ecb230034a0da92a9a1b10c0
saada <mahmoudsaada@gmail.com>
2012-06-30 18:40:18 -0700

    Handling invalid signin

--- Rails/test_project/app/controllers/sessions_controller.rb
+++ Rails/test_project/app/controllers/sessions_controller.rb
@@ -1,14 +1,24 @@
 
 class SessionsController < ApplicationController
-  def new
-  	@title = "Sign in"
-  end
   
-  def create
-  	
-  end
+	def new
+		@title = "Sign in"
+	end
 
-  def destroy
-  	
-  end
+	def create
+		user = User.authenticate(params[:session][:email],
+									params[:session][:password])
+		
+		if user.nil?
+			flash.now[:error] = "Invalid email/password combination!"
+			@title = "Sign in"
+			render 'new'
+		else
+			#Handle successful signin
+		end
+	end
+
+	def destroy
+		
+	end
 end
